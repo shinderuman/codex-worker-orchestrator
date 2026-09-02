9ea3701dacbef6a7e6e26402539b16248e53df04
shinderuman <shinderuman@gmail.com>
2026-09-02 16:25:32 +0900

    perf: expose zero-model authority reads on worker facade

--- glm-worker/cmd/glm-worker/main.go
+++ glm-worker/cmd/glm-worker/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/shinderuman/codex-worker-orchestrator/glm-worker/internal/app"
+	"github.com/shinderuman/codex-worker-orchestrator/glm-worker/internal/authoritybootstrapcmd"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--authority" {
+		if err := authoritybootstrapcmd.Execute(os.Args[2:], os.Stdout); err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		return
+	}
 	if err := app.Run(os.Args[1:]); err != nil {
-
 		_ = app.WriteProcessError(os.Stderr, err)
 		os.Exit(1)
 	}
