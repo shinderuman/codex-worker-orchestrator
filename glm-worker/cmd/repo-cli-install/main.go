e12be4f3461f49efa9e8379887de1da919db78fa
shinderuman <shinderuman@gmail.com>
2026-09-12 08:02:33 +0900

    Move repository CLI process streams behind command boundary

--- glm-worker/cmd/repo-cli-install/main.go
+++ glm-worker/cmd/repo-cli-install/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/shinderuman/codex-worker-orchestrator/glm-worker/internal/cliinstallcmd"
 )
 
 func main() {
-	if err := cliinstallcmd.Run(os.Args[1:], os.Stdout); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	os.Exit(cliinstallcmd.Main())
 }
