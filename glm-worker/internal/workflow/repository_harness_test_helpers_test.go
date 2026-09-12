2c0ac3f388a3ad08209fca3d7291bd6e705c8184
shinderuman <shinderuman@gmail.com>
2026-09-12 12:49:33 +0900

    Add repository harness test pin helper

---
+++ glm-worker/internal/workflow/repository_harness_test_helpers_test.go
@@ -0,0 +1,15 @@
+package workflow
+
+import (
+	"testing"
+
+	"github.com/shinderuman/codex-worker-orchestrator/glm-worker/internal/repositoryharness"
+	"github.com/shinderuman/codex-worker-orchestrator/glm-worker/internal/state"
+)
+
+func pinRepositoryHarnessActiveT(t *testing.T, st *state.StateStore) {
+	t.Helper()
+	if err := st.Write(repositoryharness.ActivationStateKey, repositoryharness.ActivationActiveValue); err != nil {
+		t.Fatal(err)
+	}
+}
