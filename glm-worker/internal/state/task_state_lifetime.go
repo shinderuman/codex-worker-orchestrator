432827e2a18d97283c9eb2c2d5a52ae4e498e06e
shinderuman <shinderuman@gmail.com>
2026-09-12 16:38:51 +0900

    Clear resume transition evidence on new task

--- glm-worker/internal/state/task_state_lifetime.go
+++ glm-worker/internal/state/task_state_lifetime.go
@@ -40,6 +40,7 @@ var taskBoundStatePolicies = []taskBoundStatePolicy{
 	{name: stopIndexPatchFile, lifetime: taskBoundStateFreshTaskClear},
 	{name: isolationStateFile, lifetime: taskBoundStateFreshTaskClear},
 	{name: resumeStateFile, lifetime: taskBoundStateFreshTaskClear},
+	{name: resumeTransitionFile, lifetime: taskBoundStateFreshTaskClear},
 	{name: workerEndSnapshotFile, lifetime: taskBoundStateFreshTaskClear},
 	{name: reviewStartSnapshotFile, lifetime: taskBoundStateFreshTaskClear},
 	{name: reportOnlyStartSnapshotFile, lifetime: taskBoundStateFreshTaskClear},
