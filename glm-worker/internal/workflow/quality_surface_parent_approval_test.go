74fcda620ebbf54f2429b4ff45d2931ef7f5b2e0
shinderuman <shinderuman@gmail.com>
2026-09-13 02:04:47 +0900

    Bind quality scope fixture to action invocation

--- glm-worker/internal/workflow/quality_surface_parent_approval_test.go
+++ glm-worker/internal/workflow/quality_surface_parent_approval_test.go
@@ -34,6 +34,7 @@ func TestQualitySurfaceChangeAllowsParentAcceptedCurrentDiff(t *testing.T) {
 
 	writeScopeFile(t, repo, "commentlint", "#!/bin/sh\nexport GOCACHE=/tmp/go-cache\nexit 0\n")
 	w := NewWorkflow(cfg, st, nil, io.Discard)
+	w.temp = t.TempDir()
 	if err := w.prepareAcceptedFixScopeChecked(acceptedFixScopeCurrentDiff); err != nil {
 		t.Fatal(err)
 	}
