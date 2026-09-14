7e4e9053bae8c2e923319ce108e5a4660ec1f868
shinderuman <shinderuman@gmail.com>
2026-09-14 13:35:54 +0900

    Use stats archival as the single repo search projection boundary

--- glm-worker/internal/state/store.go
+++ glm-worker/internal/state/store.go
@@ -165,7 +165,6 @@ func (s *StateStore) startNewTaskWithID(taskID string, resume bool) (string, err
 		return "", err
 	}
 
-	s.projectCurrentRepoSearchStats()
 	s.archiveCurrentStats(parentIdentity)
 	s.PruneTaskEventLogs(retainedTaskEventLogs, taskID)
 	s.InitializeTaskStats(taskID)
