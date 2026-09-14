3943cea637684bc1aec097f6d5d6c9972114a700
shinderuman <shinderuman@gmail.com>
2026-09-14 20:24:28 +0900

    Add forward-only test surface usage helpers

---
+++ glm-worker/internal/harnesslint/forward_only_test_surface_usage.go
@@ -0,0 +1,58 @@
+package harnesslint
+
+import (
+	"go/ast"
+	"go/token"
+)
+
+type forwardOnlyParsedTestFile struct {
+	path string
+	set  *token.FileSet
+	file *ast.File
+}
+
+func forwardOnlyAliasDirectlyCalled(files []forwardOnlyParsedTestFile, name string) bool {
+	for _, testFile := range files {
+		called := false
+		ast.Inspect(testFile.file, func(node ast.Node) bool {
+			call, ok := node.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+			identifier, ok := call.Fun.(*ast.Ident)
+			if ok && identifier.Name == name {
+				called = true
+				return false
+			}
+			return true
+		})
+		if called {
+			return true
+		}
+	}
+	return false
+}
+
+func forwardOnlyAliasReassigned(files []forwardOnlyParsedTestFile, name string) bool {
+	for _, testFile := range files {
+		reassigned := false
+		ast.Inspect(testFile.file, func(node ast.Node) bool {
+			assignment, ok := node.(*ast.AssignStmt)
+			if !ok {
+				return true
+			}
+			for _, target := range assignment.Lhs {
+				identifier, ok := target.(*ast.Ident)
+				if ok && identifier.Name == name {
+					reassigned = true
+					return false
+				}
+			}
+			return true
+		})
+		if reassigned {
+			return true
+		}
+	}
+	return false
+}
