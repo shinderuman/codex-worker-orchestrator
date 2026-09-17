4d8206d32945ac8253c856c2ca7654cd8c1ce325
shinderuman <shinderuman@gmail.com>
2026-09-17 09:42:40 +0900

    Add trusted auto-resume fallback context

--- glm-worker/internal/autoresume/abort_transaction.go
+++ glm-worker/internal/autoresume/abort_transaction.go
@@ -2,6 +2,28 @@ package autoresume
 
 const AutoResumeAbortAutomationUpdateUnavailable = "automation_update_unavailable"
 
+type AutoResumeFallbackPlan struct {
+	TaskID          string
+	RepoRoot        string
+	ParentThreadID  string
+	ResetAtRFC3339  string
+	ResumeAtRFC3339 string
+}
+
+func AutoResumeFallbackPlanFromToken(token string) (AutoResumeFallbackPlan, error) {
+	transaction, _, err := decodeAutoResumeTransaction(token)
+	if err != nil {
+		return AutoResumeFallbackPlan{}, err
+	}
+	return AutoResumeFallbackPlan{
+		TaskID:          transaction.TaskID,
+		RepoRoot:        transaction.RepoRoot,
+		ParentThreadID:  transaction.ParentThreadID,
+		ResetAtRFC3339:  transaction.ResetAtRFC3339,
+		ResumeAtRFC3339: transaction.ResumeAtRFC3339,
+	}, nil
+}
+
 func AbortAutoResumeTransaction(token, reason string) AutoResumeOutput {
 	transaction, transactionID, err := decodeAutoResumeTransaction(token)
 	if err != nil {
