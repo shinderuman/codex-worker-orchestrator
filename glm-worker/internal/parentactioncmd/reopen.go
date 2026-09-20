e4ac4f51fb31f11b444b4cf2761293fd1d804bdf
shinderuman <shinderuman@gmail.com>
2026-09-20 16:38:35 +0900

    fix: share parent lifecycle dispatch

--- glm-worker/internal/parentactioncmd/reopen.go
+++ glm-worker/internal/parentactioncmd/reopen.go
@@ -20,6 +20,13 @@ type reopenOutput struct {
 
 const reopenUsage = "usage: glm-parent-action reopen [--origin <origin>] [--cause <cause>]"
 
+func executeParentLifecycleAction(cfg config.AppConfig, args []string, stdout io.Writer) error {
+	if args[0] == "reopen" {
+		return executeReopen(cfg, args, stdout)
+	}
+	return executeNoGo(cfg, args, stdout)
+}
+
 func executeReopen(cfg config.AppConfig, args []string, stdout io.Writer) error {
 	origin, cause, err := reopenOptions(args[1:])
 	if err != nil {
