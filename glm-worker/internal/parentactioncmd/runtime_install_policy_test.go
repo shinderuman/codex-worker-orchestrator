b5b99c8bef3ad976393e5157150fed0a176d1e54
shinderuman <shinderuman@gmail.com>
2026-09-22 14:34:14 +0900

    test: remove parentaction-local runtime path shim

--- glm-worker/internal/parentactioncmd/runtime_install_policy_test.go
+++
@@ -1,7 +0,0 @@
-package parentactioncmd
-
-import "github.com/shinderuman/codex-worker-orchestrator/glm-worker/internal/repositoryharness"
-
-func runtimeInstallPath(path string) bool {
-	return repositoryharness.RuntimeInstallPath(path)
-}
