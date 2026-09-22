7ff3cfb0216fc72039a0e7973135044e68cae883
shinderuman <shinderuman@gmail.com>
2026-09-22 19:42:41 +0900

    refactor: remove superseded rollout chain wrapper

--- glm-worker/internal/app/bundle_codex_chain.go
+++
@@ -1,7 +0,0 @@
-package app
-
-import "github.com/shinderuman/codex-worker-orchestrator/glm-worker/internal/codexrollout"
-
-func resolveCodexRolloutChain(matches []codexRollout) ([]codexRollout, string) {
-	return codexrollout.ResolveChain(matches)
-}
