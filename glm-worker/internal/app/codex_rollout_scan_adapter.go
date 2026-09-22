7d27074451163fc69d8b607b73a3f45e9b5679db
shinderuman <shinderuman@gmail.com>
2026-09-22 20:10:51 +0900

    refactor: bind app rollout scan to neutral owner

--- glm-worker/internal/app/codex_rollout_scan_adapter.go
+++ glm-worker/internal/app/codex_rollout_scan_adapter.go
@@ -2,6 +2,4 @@ package app
 
 import "github.com/shinderuman/codex-worker-orchestrator/glm-worker/internal/codexrollout"
 
-func scanCodexRollouts(codexHome string) ([]codexRollout, error) {
-	return codexrollout.Scan(codexHome)
-}
+var scanCodexRollouts = codexrollout.Scan
