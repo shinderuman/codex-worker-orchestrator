3e5cc9aa8e977d133903ebd6d2abe68b20117207
shinderuman <shinderuman@gmail.com>
2026-09-25 01:15:31 +0900

    refactor: keep scrub in existing invocation defaults

--- glm-worker/internal/runner/claude_process_env.go
+++
@@ -1,9 +0,0 @@
-package runner
-
-const configuredClaudeSubprocessEnvScrub = "1"
-
-func claudeProcessEnvAdditions() map[string]string {
-	additions := claudeInvocationEnvDefaults()
-	additions["CLAUDE_CODE_SUBPROCESS_ENV_SCRUB"] = configuredClaudeSubprocessEnvScrub
-	return additions
-}
