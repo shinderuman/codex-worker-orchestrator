5c5696013389484540536dcbaccbbd7be309d895
shinderuman <shinderuman@gmail.com>
2026-09-25 01:33:12 +0900

    refactor: keep invocation defaults free of scrub mode

--- glm-worker/internal/runner/claude_invocation_defaults.go
+++ glm-worker/internal/runner/claude_invocation_defaults.go
@@ -4,7 +4,6 @@ import "strconv"
 
 const configuredAutoCompactWindowTokens = 500_000
 const configuredAlwaysEnableEffort = "1"
-const configuredClaudeSubprocessEnvScrub = "1"
 
 var configuredAutoCompactWindowArgument = formatAutoCompactWindowArgument(configuredAutoCompactWindowTokens)
 
@@ -15,15 +14,9 @@ func formatAutoCompactWindowArgument(tokens int) string {
 	return strconv.Itoa(tokens)
 }
 
-func claudeManagedEnvDefaults() map[string]string {
+func claudeInvocationEnvDefaults() map[string]string {
 	return map[string]string{
 		"CLAUDE_CODE_AUTO_COMPACT_WINDOW":  strconv.Itoa(configuredAutoCompactWindowTokens),
 		"CLAUDE_CODE_ALWAYS_ENABLE_EFFORT": configuredAlwaysEnableEffort,
 	}
-}
-
-func claudeInvocationEnvDefaults() map[string]string {
-	defaults := claudeManagedEnvDefaults()
-	defaults["CLAUDE_CODE_SUBPROCESS_ENV_SCRUB"] = configuredClaudeSubprocessEnvScrub
-	return defaults
 }
